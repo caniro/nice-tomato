da1f0f1caef6e5fdc8a2b31ba71f3bf4ddccb66d
Caniro <caniro@naver.com>
2021-10-06 17:03:01 +0900

    backend/mysite: Hide django secret key

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from secret_key import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#0(5p1))t5@pxkcnw9d%x#mi$rt*u&s08u*4amjz3in#u2r^-@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
