76669c194c0f64df8812e4c33d811390d8a29f87
Caniro <caniro@naver.com>
2021-10-15 14:38:41 +0900

    backend: Add queryset and serializer for calculating average of sensor value

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,6 +3,10 @@ from mqtt.models import Sensor
 
 # Django의 모델 -> json 형식으로 직렬화
 class SensorSerializer(serializers.ModelSerializer):
+    count = serializers.IntegerField()
+    avg = serializers.FloatField()
+    regdate_h = serializers.DateTimeField()
+
     class Meta:
         model = Sensor
-        exclude = []
+        fields = ('place', 'section', 'sensor', 'regdate_h', 'count', 'avg')
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,38 @@
+from django.db.models import Avg, Sum, Count
+from django.db.models.functions import Trunc
 from rest_framework import viewsets
 from mqtt.models import Sensor
 from .serializers import SensorSerializer
 from .paginations import SensorPageNumberPagination
 from rest_framework.permissions import AllowAny
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class SensorViewSet(viewsets.ModelViewSet):
-    now = datetime.today()
-    start_time = "2021-10-14T03:00:00"
-    queryset = Sensor.objects.filter(regdate__range=[start_time, now]).order_by('-regdate')
+    queryset = Sensor.objects.all() # 성능 이슈 있는지?
     serializer_class = SensorSerializer
-    pagination_class = SensorPageNumberPagination
-    permission_classes = (AllowAny, ) # jwt 인증 제외
+    # pagination_class = SensorPageNumberPagination
+    # permission_classes = (AllowAny, )
+
+    # now = datetime.today()
+    # start_time = now - timedelta(hours=12)
+    # queryset = Sensor.objects\
+    #     .filter(regdate__gte=start_time)\
+    #     .filter(place='farm', section='section1')\
+    #     .annotate(regdate_h=Trunc('regdate', 'hour'))\
+    #     .values('place', 'section', 'sensor', 'regdate_h')\
+    #     .annotate(count=Count('id'), avg=Avg('value'))
+    # print(queryset)
+
+    def get_queryset(self):
+        now = datetime.today()
+        start_time = now - timedelta(hours=12)
+        place = self.request.query_params.get('place', '')
+        section = self.request.query_params.get('section', '')
+        queryset = Sensor.objects\
+            .filter(regdate__gte=start_time, place=place, section=section)\
+            .annotate(regdate_h=Trunc('regdate', 'hour'))\
+            .values('place', 'section', 'sensor', 'regdate_h')\
+            .annotate(count=Count('id'), avg=Avg('value'))
+
+        return queryset
+ 
